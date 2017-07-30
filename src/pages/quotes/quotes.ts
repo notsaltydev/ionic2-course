190d86cc86a31dbb367cd3d5d625ac263ca9a07a
adamnieslony <notsaltydev@gmail.com>
2017-07-30 14:26:40 +0200

    47. How to create more complex List Items

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuotesPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-quotes',
@@ -14,11 +7,5 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class QuotesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
-  }
 
 }
