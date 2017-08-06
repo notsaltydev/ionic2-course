7a3e05a2e0da9c9d676a31aadd8dda2d10fd750f
adamnieslony <notsaltydev@gmail.com>
2017-08-06 13:23:23 +0200

    59. Dismissing Modals

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ViewController} from 'ionic-angular';
 
 /**
  * Generated class for the QuotePage page.
@@ -14,11 +14,10 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class QuotePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private viewCtrl: ViewController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+  onClose() {
+    this.viewCtrl.dismiss();
   }
-
 }
